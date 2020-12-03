9a8997e27c51814208b31042f42cb6a20de75cef
Lachee <lachlan.h@hotmail.com>
2020-12-03 15:52:14 +1100

    Force the route to be lowercase

--- src/models/CronTask.php
+++ src/models/CronTask.php
@@ -44,6 +44,7 @@ class CronTask extends ActiveRecord
             [['schedule', 'route'], 'string', 'max' => 255],
             [['schedule'], 'match', 'pattern' => self::SCHEDULE_REGEX],
             [['schedule', 'route'], 'required'],
+            [['route']], 'filter', 'filter' => 'strtolower'],
         ];
     }
 
